b4d1564cb39763a2ad2e5d4b1291af372ab56771
Tommy Lukmana <tl27863@proton.me>
2025-01-21 10:06:51 +0700

    get all stream(without user)

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const userRoute = require("./routes/userRoute");
+const streamRoute = require("./routes/streamRoute");
 const cors = require("cors");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
@@ -14,13 +15,14 @@ app.use(
     message: "You have exceeded the 100 requests in 1 hour limit!",
     standarHeaders: true,
     legacyHeaders: true,
-  })
+  }),
 );
 
 app.use(cors());
 
 app.use(express.json());
 app.use("/user", userRoute);
+app.use("/stream", streamRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
