2563e50d1827523c15f9425a4dfec63f8752cf3d
Tommy Lukmana <tl27863@proton.me>
2025-01-23 09:45:49 +0700

    get user livestream link route

--- backend/routes/streamRoute.js
+++ backend/routes/streamRoute.js
@@ -5,6 +5,7 @@ const ORYX_APIURL = "http://localhost:2022/api/v1";
 const ORYX_EMBEDURL = "http://localhost:2022";
 const { apiDataSource } = require("../config/db");
 const { User } = require("../entities/user");
+const { Streamkey } = require("../entities/streamkey");
 
 //Get All Stream from oryx
 router.get("/", async (req, res) => {
@@ -53,10 +54,28 @@ router.get("/", async (req, res) => {
   }
 });
 
-//Get user streamlink
+//Get user livestream link
 router.get("/:id", async (req, res) => {
   try {
-  } catch (error) {}
+    const { id } = req.params;
+    const validation = await apiDataSource.getRepository(Streamkey).findOne({
+      where: { userId: id },
+    });
+
+    if (validation) {
+      const result = await apiDataSource.getRepository(User).findOne({
+        where: { id: id },
+      });
+      res.status(200).json({
+        streamUrl: `${ORYX_EMBEDURL}/live/${result.username}/${validation.key.split("?")[0]}.m3u8`,
+      });
+    }
+    return res
+      .status(404)
+      .json({ message: "User never / havent started streaming!" });
+  } catch (error) {
+    console.error("Error:", error);
+  }
 });
 
 module.exports = router;
